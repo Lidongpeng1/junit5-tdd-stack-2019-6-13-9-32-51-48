f8d8d303a53ffb8b662f539c4521775488990c07
LiDongpeng1 <1609640094@qq.com>
2019-07-12 16:26:15 +0800

    test9.... give anynumber return is right

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -2,14 +2,22 @@ package com.thoughtworks.tdd;
 
 public class FizzBuzz {
     public String getResult(Integer number) {
+
+        String result = "";
+
         if (number % 3 == 0) {
-            return "Fizz";
-        } else if (number % 5 == 0) {
-            return "Buzz";
-        } else if (number % 7 == 0) {
-            return "Whizz";
+            result.concat("Fizz");
+        }
+        if (number % 5 == 0) {
+            result.concat("Buzz");
+        }
+        if (number % 7 == 0) {
+            result.concat("Whizz");
+        }
+        if (result == ""){
+            result = number.toString();
         }
 
-        return number.toString();
+        return result;
     }
 }
--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -9,7 +9,7 @@ import static org.mockito.Mockito.when;
 
 public class FizzBuzzTest {
     @Test
-    public void give_1_should_return_1() {
+    public void should_return_1_when_number_is_1() {
         //given
         Integer number = 1;
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -22,7 +22,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void give_2_should_return_2() {
+    public void should_return_2_when_number_is_2() {
         //given
         Integer number = 2;
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -35,7 +35,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void give_3_should_return_Fizz() {
+    public void should_return_fizz_when_number_is_3() {
         //given
         Integer number = 3;
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -48,7 +48,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void give_6_should_return_Fizz() {
+    public void should_return_fizz_when_number_is_6() {
         //given
         Integer number = 6;
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -61,7 +61,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void give_5_should_return_Buzz() {
+    public void should_return_buzz_when_number_is_5() {
         //given
         Integer number = 5;
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -74,7 +74,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void give_10_should_return_Buzz() {
+    public void should_return_buzz_when_number_is_10() {
         //given
         Integer number = 10;
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -87,7 +87,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void give_7_should_return_Whizz() {
+    public void should_return_whizz_when_number_is_7() {
         //given
         Integer number = 7;
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -100,7 +100,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void give_14_should_return_Whizz() {
+    public void should_return_whizz_when_number_is_14() {
         //given
         Integer number = 14;
         FizzBuzz fizzBuzz = new FizzBuzz();
